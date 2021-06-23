e7a339eafbb8d11e93ffcb0c7df1f8b7a3aaf682
Atef <mahmoudbenatef@gmail.com>
2021-06-23 17:28:55 +0200

    edit category

--- src/routes/campRoute.js
+++ src/routes/campRoute.js
@@ -1,11 +1,12 @@
 const express = require("express")
-const { createOne, getAll, deleteOne, updateOne, getAvailCamps, registerUser } = require('../controllers/campController');
+const { createOne, getAll, deleteOne, updateOne, getAvailCamps, registerUser, getCamp } = require('../controllers/campController');
 const { isAdmin } = require("../middlewares/AdminMiddleware")
 const paginateMode = require("../middlewares/paginateModel");
 const CampModel = require("../models/campModel");
 Router = express.Router();
 Router.route("/").post(isAdmin, createOne).get(paginateMode(CampModel), getAll);
-Router.route("/:id").delete(isAdmin, deleteOne).put(isAdmin, updateOne);
+Router.route("/:id").delete(isAdmin, deleteOne).put(isAdmin, updateOne)
+    .get(isAdmin, getCamp);
 Router.route("/availCamps").get(getAvailCamps);
 Router.get("/:campID/:userID", registerUser);
 module.exports = Router;
