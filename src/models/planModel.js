6fe498a07014d46e90b9023084864cf9470f05ff
HatemHashem <hatemsayed001@gmail.com>
2021-06-24 00:45:18 +0200

    add user plan routes and logic

--- src/models/planModel.js
+++ src/models/planModel.js
@@ -1,47 +1,49 @@
 const mongoose = require("mongoose");
+let currentDate = new Date();
+currentDate.setUTCHours(0, 0, 0, 0);
 const PlanScheme = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
-    },
-    day: {
-        type: Date,
-        default: Date.now()
-    },
-    breakfast: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Meal",
-        required: true,
-    },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  day: {
+    type: Date,
+    default: currentDate.toISOString(),
+  },
+  breakfast: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Meal",
+    required: true,
+  },
 
-    breakfastCheck: {
-        type: Boolean,
-        default: false,
-    },
-    launch: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Meal",
-        required: true,
-    },
+  breakfastCheck: {
+    type: Boolean,
+    default: false,
+  },
+  launch: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Meal",
+    required: true,
+  },
 
-    launchCheck: {
-        type: Boolean,
-        default: false,
-    },
-    dinner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Meal",
-        required: true,
-    },
-    dinnerCheck: {
-        type: Boolean,
-        default: false,
-    },
-    created: {
-        type: Date,
-        default: Date.now,
-    },
+  launchCheck: {
+    type: Boolean,
+    default: false,
+  },
+  dinner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Meal",
+    required: true,
+  },
+  dinnerCheck: {
+    type: Boolean,
+    default: false,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
 });
 const PlanModel = mongoose.model("Plan", PlanScheme);
 module.exports = PlanModel;
--- src/routes/planRoute.js
+++ src/routes/planRoute.js
@@ -1,6 +1,9 @@
 const express = require("express")
-const { createOne, } = require('../controllers/planContrleer');
+const { createOne,getOne,updateOne } = require('../controllers/planContrleer');
 const { isAdmin } = require("../middlewares/AdminMiddleware")
 const Router = express.Router();
-Router.route("/").post(isAdmin, createOne)
+Router.route("/").post(isAdmin, createOne);
+Router.route("/:id/:date").get(getOne);
+Router.route("/:id/:date").patch(updateOne);
+
 module.exports = Router;
