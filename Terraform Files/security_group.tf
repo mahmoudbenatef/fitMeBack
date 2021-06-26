22e5d802c72e8a1070e34ef83e54fb3b61f90a46
Mohamedelbeshlawy <67645325+Mohamedelbeshlawy@users.noreply.github.com>
2021-06-26 23:34:39 +0200

    Update security_group.tf

--- Terraform Files/security_group.tf
+++ Terraform Files/security_group.tf
@@ -8,6 +8,18 @@ resource "aws_security_group" "first-security-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
