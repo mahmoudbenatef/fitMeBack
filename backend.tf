f471bc5fab3c44ece0499c40c17ceaa11ead8cb5
Mohamedelbeshlawy <67645325+Mohamedelbeshlawy@users.noreply.github.com>
2021-06-26 14:05:21 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "vpc-bucket-gp"
+    bucket         = "vpc-bucket-gp-iti"
     key            = "dev/terraform.tfstate"
-    region         = "us-east-2"
+    region         = "eu-central-1"
     dynamodb_table = "mytable-terraform-gp"
   }
 }
