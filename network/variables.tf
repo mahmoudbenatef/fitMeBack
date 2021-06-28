16353d9b869af7df70c2ef315092c9aeb3aa8be8
Mohamedelbeshlawy <67645325+Mohamedelbeshlawy@users.noreply.github.com>
2021-06-28 23:31:02 +0200

    Update variables.tf

--- network/variables.tf
+++ network/variables.tf
@@ -10,18 +10,6 @@ variable "public-1_subnet_cidr" {
   type = string
 }
 
-variable "public-2_subnet_cidr" {
-  type = string
-}
-
-variable "private-1_subnet_cidr" {
-  type = string
-}
-
-variable "private-2_subnet_cidr" {
-  type = string
-}
-
 variable "AZ-1" {
   type = string
 }
