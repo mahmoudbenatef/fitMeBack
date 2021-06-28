98f04017046632bab67a49e891548e9f39556ff1
Mohamedelbeshlawy <67645325+Mohamedelbeshlawy@users.noreply.github.com>
2021-06-28 23:16:51 +0200

    Update outputs.tf

--- Terraform Files/network/outputs.tf
+++ Terraform Files/network/outputs.tf
@@ -2,18 +2,6 @@ output "public-1_id" {
   value = aws_subnet.public-1.id
 }
 
-output "public-2_id" {
-  value = aws_subnet.public-2.id
-}
-
-output "private-1_id" {
-  value = aws_subnet.private-1.id
-}
-
-output "private-2_id" {
-  value = aws_subnet.private-2.id
-}
-
 output "vpc_id" {
     value = aws_vpc.mainVPC.id
 }
