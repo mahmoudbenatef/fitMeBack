35e02517f2e6efa737feb100136d280ee3e2cf62
Mohamedelbeshlawy <67645325+Mohamedelbeshlawy@users.noreply.github.com>
2021-06-28 23:30:16 +0200

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -1,25 +1,14 @@
-resource "aws_instance" "bastion" {
+resource "aws_instance" "App" {
   ami                    = var.ami
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.newPrivateKey.key_name
   vpc_security_group_ids = [aws_security_group.first-security-group.id]
   subnet_id              = module.networkMod.public-1_id
   tags = {
-    "Name" = "bastionInstance"
+    "Name" = "App"
   }
 
   provisioner "local-exec" {
     command = "echo The server IP address is ${self.public_ip}"
-  }
-}
-
-resource "aws_instance" "private" {
-  ami                    = var.ami
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.newPrivateKey.key_name
-  vpc_security_group_ids = [aws_security_group.second-security-group.id]
-  subnet_id              = module.networkMod.private-1_id
-  tags = {
-    "Name" = "privateInstance"
   }
 }
