968fb53d83aef40cd396a5c0a8246034701bce79
Mohamedelbeshlawy <67645325+Mohamedelbeshlawy@users.noreply.github.com>
2021-06-28 23:12:47 +0200

    Update main.tf

--- Terraform Files/main.tf
+++ Terraform Files/main.tf
@@ -3,9 +3,6 @@ module "networkMod" {
   cidr                  = var.cidr
   name                  = var.name
   public-1_subnet_cidr  = var.public-1_subnet_cidr
-  public-2_subnet_cidr  = var.public-2_subnet_cidr
-  private-1_subnet_cidr = var.private-1_subnet_cidr
-  private-2_subnet_cidr = var.private-2_subnet_cidr
   AZ-1                  = var.AZ-1
   AZ-2                  = var.AZ-2
 }
