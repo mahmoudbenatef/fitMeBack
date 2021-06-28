0edb4acf0de294766794b31ea73a2a23e910220f
Mohamedelbeshlawy <67645325+Mohamedelbeshlawy@users.noreply.github.com>
2021-06-28 23:11:45 +0200

    Update security_group.tf

--- Terraform Files/security_group.tf
+++ Terraform Files/security_group.tf
@@ -15,8 +15,8 @@ resource "aws_security_group" "first-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 3001
+    to_port     = 3001
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -27,28 +27,3 @@ resource "aws_security_group" "first-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group" "second-security-group" {
-  name        = "second-security-group"
-  description = "Second Security Group"
-  vpc_id      = module.networkMod.vpc_id
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
